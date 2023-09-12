77b5d11fe9e6f5ae8e02c172ccbae39df1a0295f
CristianHortua98 <alejo.hortua3@gmail.com>
2023-09-12 12:10:06 -0500

    Se ajuste conexion

--- modelos/conexion.php
+++ modelos/conexion.php
@@ -7,9 +7,9 @@ class Conexion{
 
 
         //LOCAL
-        $link = new PDO("mysql:host=127.0.0.1:3306;dbname=api",
-        "root",
-        "9805");
+        $link = new PDO("mysql:host=api.mysql.database.azure.com:3306;dbname=api",
+        "adminapi",
+        "Cristian9805*");
 
         $link->exec("set names utf8");
 
